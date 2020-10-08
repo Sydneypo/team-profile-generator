5d05b7afaf3651b524a16e707109f33708b0ef00
Murphy <sydneyporter74@yahoo.com>
2020-10-08 18:19:23 -0400

    finished inquirer questions

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -7,3 +7,21 @@ test('creates an employee object', () => {
     expect(employee.id).toBe('1234');
     expect(employee.email).toBe('john@john.com');
 });
+
+test("gets employee's name", () => {
+    const employee = new Employee('John Smith', '1234', 'john@john.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining('John Smith'));
+});
+
+test("gets employee's id number", () => {
+    const employee = new Employee('John Smith"', '1234', 'john@john.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining('1234'));
+});
+
+test("gets employee's email address", () => {
+    const employee = new Employee('John Smith', '1234', 'john@john.com');
+    
+    expect(employee.getEmail()).toEqual(expect.stringContaining('john@john.com'));
+});
